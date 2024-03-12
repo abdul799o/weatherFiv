48f1f48e7c425da9ecdc84fc6d450d2ebe2906a9
Nasrudin Omar <n.omar@se22.qmul.ac.uk>
2024-03-12 18:29:53 +0000

    Update Weather.js
    weather map added

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,11 @@ import './Weather.css';
 
 const Weather = () => {
 const [city, setCity] = useState('');
+const z = 0;
+const x = 0;
+const y = 0;
 const [weatherData, setWeatherData] = useState(null);
+const [weatherMap, setWeatherMap] = useState(null); 
 const fetchData = async () => {
 try {
 const response = await axios.get(
@@ -15,6 +19,21 @@ console.log(response.data); //You can see all the weather data in console log
 } catch (error) {
 console.error(error);
 }
+try {
+    const reply = await axios.get(
+    `https://tile.openweathermap.org/map/temp_new/${z}/${x}/${y}.png?appid=69084501b8a41087da148556d2a1b461`
+    );
+    fetch(`https://tile.openweathermap.org/map/temp_new/${z}/${x}/${y}.png?appid=69084501b8a41087da148556d2a1b461`)
+    .then(reply => {return reply.blob()})
+    .then(blob => {
+        var img = URL.createObjectURL(blob);
+        console.log(img)
+        setWeatherMap(img);
+    })
+    console.log(reply.data); //You can see all the weather data in console log
+    } catch (error) {
+    console.error(error);
+    }
 };
 useEffect(() => {
 fetchData();
@@ -53,6 +72,8 @@ onChange={handleInputChange}
 <p>Humidity : {weatherData.main.humidity}%</p>
 <p>Pressure : {weatherData.main.pressure}</p>
 <p>Wind Speed : {weatherData.wind.speed}m/s</p>
+<p>{weatherMap}</p>
+<img src = {weatherMap} alt='map'width={1000} height={800} ></img>
 </>
 ) : (
 <p>Loading weather data...</p>
